bd9f7bb77b92ce0d17c7203354d587172a20c9cd
Rohit Wadi <rohitwadi1@gmail.com>
2023-11-28 16:29:32 +0000

    added index file

--- FoodProject/FoodProject/urls.py
+++ FoodProject/FoodProject/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Foodapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # ---- HOME ----
+    path('',views.foodapp),
+    path('home',views.foodapp),
 ]
--- FoodProject/Foodapp/views.py
+++ FoodProject/Foodapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import connection, transaction
+from Foodapp.forms import FoodForm,CustForm,AdminForm,CartForm,OrderForm
+from Foodapp.models import Food,Cust,Admin,Cart,Order
+import datetime
+
+cursor = connection.cursor()
 
 # Create your views here.
+
+
+def foodapp(request):
+    return render(request,'index.html')
